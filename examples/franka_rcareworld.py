163605602901101a63371bf7feba2df299242107
Pranav Thakkar <pnt8@cornell.edu>
2024-02-27 17:19:35 -0500

    incorporated content (before while loop) from Rajat's Storm repo

--- examples/franka_rcareworld.py
+++ examples/franka_rcareworld.py
@@ -55,6 +55,7 @@ from quaternion import from_euler_angles, as_float_array, as_rotation_matrix, fr
 # from storm_kit.gym.sim_robot import RobotSim
 # from storm_kit.util_file import get_configs_path, get_gym_configs_path, join_path, load_yaml, get_assets_path
 # from storm_kit.gym.helpers import load_struct_from_dict
+from storm_kit.util_file import get_gym_configs_path, join_path, load_yaml
 
 from storm_kit.util_file import get_mpc_configs_path as mpc_configs_path
 
@@ -77,77 +78,24 @@ def mpc_robot_interactive(args, sim_params):
     storm2unity_rot = np.array([[0, 0, 1], [-1, 0, 0], [0, 1, 0]])
     unity2storm_rot = np.linalg.inv(storm2unity_rot)
 
-    # gym = gym_instance.gym
-    # sim = gym_instance.sim
-    # world_yml = join_path(get_gym_configs_path(), world_file)
-    # with open(world_yml) as file:
-    #     world_params = yaml.load(file, Loader=yaml.FullLoader)
-
-    # robot_yml = join_path(get_gym_configs_path(),args.robot + '.yml')
-    # with open(robot_yml) as file:
-    #     robot_params = yaml.load(file, Loader=yaml.FullLoader)
-    # sim_params = robot_params['sim_params']
-    # sim_params['asset_root'] = get_assets_path()
+    env = RCareStorm()
 
     if(args.cuda):
         device = 'cuda'
     else:
         device = 'cpu'
 
-    # sim_params['collision_model'] = None
-    # # create robot simulation:
-    # robot_sim = RobotSim(gym_instance=gym, sim_instance=sim, **sim_params, device=device)
-
-    
-    # # create gym environment:
-    # robot_pose = sim_params['robot_pose']
-    # env_ptr = gym_instance.env_list[0]
-    # robot_ptr = robot_sim.spawn_robot(env_ptr, robot_pose, coll_id=2)
-
     device = torch.device('cuda', 0) 
 
     tensor_args = {'device':device, 'dtype':torch.float32}
-    
 
-    # # spawn camera:
-    # robot_camera_pose = np.array([1.6,-1.5, 1.8,0.707,0.0,0.0,0.707])
-    # q = as_float_array(from_euler_angles(-0.5 * 90.0 * 0.01745, 50.0 * 0.01745, 90 * 0.01745))
-    # robot_camera_pose[3:] = np.array([q[1], q[2], q[3], q[0]])
-
-    
-    # robot_sim.spawn_camera(env_ptr, 60, 640, 480, robot_camera_pose)
-
-    # # get pose
-    # w_T_r = copy.deepcopy(robot_sim.spawn_robot_pose)
-    
-    # w_T_robot = torch.eye(4)
-    # quat = torch.tensor([w_T_r.r.w,w_T_r.r.x,w_T_r.r.y,w_T_r.r.z]).unsqueeze(0)
-    # rot = quaternion_to_matrix(quat)
-    # w_T_robot[0,3] = w_T_r.p.x
-    # w_T_robot[1,3] = w_T_r.p.y
-    # w_T_robot[2,3] = w_T_r.p.z
-    # w_T_robot[:3,:3] = rot[0]
-
-    # world_instance = World(gym, sim, env_ptr, world_params, w_T_r=w_T_r)
-    
-
-    
-    # table_dims = np.ravel([1.5,2.5,0.7])
-    # cube_pose = np.ravel([0.35, -0.0,-0.35,0.0, 0.0, 0.0,1.0])
-    
-
-
-    # cube_pose = np.ravel([0.9,0.3,0.4, 0.0, 0.0, 0.0,1.0])
-    
-    # table_dims = np.ravel([0.35,0.1,0.8])
-
-    
-    
-    # cube_pose = np.ravel([0.35,0.3,0.4, 0.0, 0.0, 0.0,1.0])
-    
-    # table_dims = np.ravel([0.3,0.1,0.8])
-    
-    env = RCareStorm()
+    w_T_robot = torch.eye(4)
+    quat = torch.tensor([1,0,0,0]).unsqueeze(0)
+    rot = quaternion_to_matrix(quat)
+    w_T_robot[0,3] = 0
+    w_T_robot[1,3] = 0
+    w_T_robot[2,3] = 0
+    w_T_robot[:3,:3] = rot[0]
 
     # get camera data:
     mpc_control = ReacherTask(task_file, robot_file, world_file, tensor_args)
@@ -155,75 +103,36 @@ def mpc_robot_interactive(args, sim_params):
     start_qdd = torch.zeros(n_dof, **tensor_args)
 
     # update goal:
+
     exp_params = mpc_control.exp_params
+    
     ee_list = []
     
 
     mpc_tensor_dtype = {'device':device, 'dtype':torch.float32}
 
     tgt_p = env.get_target_eef_pose()
+    g_pos_ = tgt_p['position']
+    transform = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+    g_pos = np.matmul(unity2storm_rot, g_pos_)
+
+    # Rajat TODO: Verify rotation convention
+    g_q = R.from_euler('yxz', tgt_p['orientation'], degrees=True).as_quat() 
     
-    #convert eef position in Unity to Storm
-    g_pos = np.matmul(unity2storm_rot, tgt_p['position'])
-
-    # franka_bl_state = np.array([-0.3, 0.3, 0.2, -2.0, 0.0, 2.4,0.0,
-    #                             0.0,0.0,0.0,0.0,0.0,0.0,0.0])
-    # x_des_list = [franka_bl_state]
+    ee_error = 10.0
+    j = 0
+    t_step = 0
+    i = 0
     
-    # ee_error = 10.0
-    # j = 0
-    # t_step = 0
-    # i = 0
-    # x_des = x_des_list[0]
-    
-    # mpc_control.update_params(goal_state=x_des)
-
-    # # spawn object:
-    # x,y,z = 0.0, 0.0, 0.0
-    # tray_color = gymapi.Vec3(0.8, 0.1, 0.1)
-    # asset_options = gymapi.AssetOptions()
-    # asset_options.armature = 0.001
-    # asset_options.fix_base_link = True
-    # asset_options.thickness = 0.002
-
-
-    # object_pose = gymapi.Transform()
-    # object_pose.p = gymapi.Vec3(x, y, z)
-    # object_pose.r = gymapi.Quat(0,0,0, 1)
-    
-    # obj_asset_file = "urdf/mug/movable_mug.urdf" 
-    # obj_asset_root = get_assets_path()
-    
-    # if(vis_ee_target):
-    #     target_object = world_instance.spawn_object(obj_asset_file, obj_asset_root, object_pose, color=tray_color, name='ee_target_object')
-    #     obj_base_handle = gym.get_actor_rigid_body_handle(env_ptr, target_object, 0)
-    #     obj_body_handle = gym.get_actor_rigid_body_handle(env_ptr, target_object, 6)
-    #     gym.set_rigid_body_color(env_ptr, target_object, 0, gymapi.MESH_VISUAL_AND_COLLISION, tray_color)
-    #     gym.set_rigid_body_color(env_ptr, target_object, 6, gymapi.MESH_VISUAL_AND_COLLISION, tray_color)
-
-
-    #     obj_asset_file = "urdf/mug/mug.urdf"
-    #     obj_asset_root = get_assets_path()
-
-
-    #     ee_handle = world_instance.spawn_object(obj_asset_file, obj_asset_root, object_pose, color=tray_color, name='ee_current_as_mug')
-    #     ee_body_handle = gym.get_actor_rigid_body_handle(env_ptr, ee_handle, 0)
-    #     tray_color = gymapi.Vec3(0.0, 0.8, 0.0)
-    #     gym.set_rigid_body_color(env_ptr, ee_handle, 0, gymapi.MESH_VISUAL_AND_COLLISION, tray_color)
-
+    mpc_control.update_params(goal_ee_pos=g_pos, goal_ee_quat=g_q)
 
     g_pos = np.ravel(mpc_control.controller.rollout_fn.goal_ee_pos.cpu().numpy())
     
     g_q = np.ravel(mpc_control.controller.rollout_fn.goal_ee_quat.cpu().numpy())
-    # object_pose.p = gymapi.Vec3(g_pos[0], g_pos[1], g_pos[2])
 
-    # object_pose.r = gymapi.Quat(g_q[1], g_q[2], g_q[3], g_q[0])
-    # object_pose = w_T_r * object_pose
-    # if(vis_ee_target):
-    #     gym.set_rigid_transform(env_ptr, obj_base_handle, object_pose)
     n_dof = mpc_control.controller.rollout_fn.dynamics_model.n_dofs
     prev_acc = np.zeros(n_dof)
-    ee_pose = gymapi.Transform()
+    
     w_robot_coord = CoordinateTransform(trans=w_T_robot[0:3,3].unsqueeze(0),
                                         rot=w_T_robot[0:3,0:3].unsqueeze(0))
 
@@ -243,6 +152,9 @@ def mpc_robot_interactive(args, sim_params):
 
     t_now = time.time()
 
+    qdd_des = np.array([0,0,0,0,0,0,1])
+    # env.set_robot_joint_position(joint_positions=(np.array([-0.9693274502997367, 0.9422471412712752, 0.7439066754002944, 1.019769135341833, 1.672839590475963, -1.3861354591630555])*180/np.pi))
+
     while True:
         try:
             tgt_p = env.get_target_eef_pose()
@@ -252,7 +164,11 @@ def mpc_robot_interactive(args, sim_params):
             # transform = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
             g_pos = np.matmul(unity2storm_rot, tgt_p['position'])
 
-            storm_frame_orientation = [-tgt_p['orientation'][2], tgt_p['orientation'][0], -tgt_p['orientation'][1]]
+            # storm_frame_orientation = [-tgt_p['orientation'][2], tgt_p['orientation'][0], -tgt_p['orientation'][1]]
+            
+            #trying this for now
+            storm_frame_orientation = tgt_p['orientation']
+
             print("Updated Euler:",storm_frame_orientation)
             g_q = R.from_euler('xyz', storm_frame_orientation, degrees=True).as_quat()
             storm_q = [g_q[3], g_q[0], g_q[1], g_q[2]]
